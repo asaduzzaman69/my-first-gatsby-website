3ab688bac3f3595db684a2f8bc37a4b6c653e535
asaduzzaman69 <asaduzzamanhimel12@gmail.com>
2021-08-20 10:59:59 +0600

    Finished Gatsby Tutorial Part 3

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,15 +1,20 @@
 // Step 1: Import React
-import { Link } from 'gatsby'
-import * as React from 'react'
-import Layout from '../components/layout'
+import { Link } from "gatsby";
+import * as React from "react";
+import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
+
 // Step 2: Define your component
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
-    <p>I'm making this by following the Gatsby Tutorial.</p>
-  </Layout>
-
-  )
-}
+      <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        src="./../images/tree.jpg"
+        alt="tree with the nature"
+      />
+    </Layout>
+  );
+};
 // Step 3: Export your component
-export default IndexPage
+export default IndexPage;
